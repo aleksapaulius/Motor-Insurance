ea9fe7929db38e99c2bd5e3ad9604f4ef3fea7a1
aleksapaulius <apauliui@gmail.com>
2019-05-11 10:38:28 +0300

    .

--- 00_main.R
+++ 00_main.R
@@ -11,11 +11,12 @@ library("ggplot2")
 library("ggcorrplot")
 
 
-# input
-mdata <- read.csv2(file = "input/Data.csv", sep = ",", dec = '.')
+### input
+mdata.input <- read.csv2(file = "input/Data.csv", sep = ",", check.names = F, dec = '.')
+mdata <- mdata.input
 
 
-# Descriptive statistics
+### Descriptive statistics
 ls.str(mdata)
 hist(mdata$claim_count, breaks = 50) 
 mean(mdata$claim_count) 
@@ -23,16 +24,39 @@ var(mdata$claim_count)
 table(mdata$claim_count) 
 
 
-# Data preparation/cleaning
-# Change type of variables
-mdata$num_exposure <- suppressWarnings(as.numeric(as.character(mdata$num_exposure)))
-# remove NULL values
+### Data preparation/cleaning
+
+# Remove 'policy_desc' column
+mdata <- mdata[,!(names(mdata) %in% c('policy_desc'))]
+
+# Renaming variables
+names(mdata)[names(mdata) == "cat_areacode"] <- "areacode"
+names(mdata)[names(mdata) == "num_vehicleAge"] <- "vehicle_age"
+names(mdata)[names(mdata) == "num_noClaimDiscountPercent"] <- "discount"
+names(mdata)[names(mdata) == "cat_carBrand"] <- "car_brand"
+names(mdata)[names(mdata) == "num_populationDensitykmsq"] <- "population_density"
+names(mdata)[names(mdata) == "cat_Region"] <- "region"
+names(mdata)[names(mdata) == "ord_vehicleHP"] <- "vehicle_hp"
+names(mdata)[names(mdata) == "num_exposure"] <- "exposure"
+names(mdata)[names(mdata) == "cat_fuelType"] <- "fuel_type"
+names(mdata)[names(mdata) == "num_driverAge"] <- "driver_age"
+
+# Change type of variable 'exposure'
+mdata$exposure <- suppressWarnings(as.numeric(as.character(mdata$exposure)))
+
+# Remove rows with missing values
+any(!complete.cases(mdata))
+mdata <- mdata[complete.cases(mdata), ]
+
+# Remove rows where variable 'fuel_type' is NULL
+mdata <- mdata[-which(mdata[,'fuel_type'] == 'NULL'), ]
 
 
-# Correlation
+### Correlation
 source("01_correlation.R")
 
-# Model
+
+### Model
 source("02_model.R")
 
 
--- 02_model.R
+++ 02_model.R
@@ -3,40 +3,58 @@ model.stat <- matrix(0, ncol = 4, nrow = 1) %>% data.frame()
 names(model.stat) <- c('RSE', 'Adjusted R-squared', 'F-Statistic', 'any-aliased')
 
 
-# MODEL 1. Linear model
+### Linear model
+
+# MODEL 1
 lm.regressors1 <- setdiff(names(mdata), c('claim_count', 'policy_desc'))
 f1 <- as.formula(paste("claim_count ~ ", paste(lm.regressors1, collapse=" + ")))
 fit1 <- lm(f1, data = mdata)
+summary(fit1)
 model.stat.fit1 <-  model.stat
 model.stat.fit1$RSE <- summary(fit1)$sigma
 model.stat.fit1$`Adjusted R-squared` <- summary(fit1)$adj.r.squared
 model.stat.fit1$`F-Statistic` <- summary(fit1)$fstatistic[1]
 model.stat.fit1$`any-aliased` <- any(summary(fit1)$aliased)
 
-
-
-# MODEL 2. Poisson regression
-lm.regressors2 <- setdiff(names(mdata), c('claim_count', 'policy_desc'))
+# MODEL 2
+lm.regressors2 <- setdiff(names(mdata), c('claim_count', 'policy_desc', 'population_density', 'vehicle_hp'))
 f2 <- as.formula(paste("claim_count ~ ", paste(lm.regressors2, collapse=" + ")))
+fit2 <- lm(f2, data = mdata)
+summary(fit2) # higher F-Statistic shows stronger relashionship between exogenic and endogenic variables
+model.stat.fit2 <-  model.stat
+model.stat.fit2$RSE <- summary(fit2)$sigma
+model.stat.fit2$`Adjusted R-squared` <- summary(fit2)$adj.r.squared
+model.stat.fit2$`F-Statistic` <- summary(fit2)$fstatistic[1]
+model.stat.fit2$`any-aliased` <- any(summary(fit2)$aliased)
 
-poisson.model <- glm(f2, mdata, family = poisson(link = "log"))
-summary(poisson.model)
 
-poisson.model2 <- glm(cf2, mdata, family = quasipoisson(link = "log"))
-summary(poisson.model2)
+### Poisson regression
+
+# MODEL 3
+pr.regressors3 <- setdiff(names(mdata), c('claim_count', 'policy_desc'))
+f3 <- as.formula(paste("claim_count ~ ", paste(pr.regressors3, collapse=" + ")))
+fit3 <- glm(f3, mdata, family = poisson(link = "log"))
+summary(fit3)
+
+f4 <- f3
+fit4 <- glm(f4, mdata, family = quasipoisson(link = "log"))
+summary(fit4)
 
 # Comparing The Models
-coef1=coef(poisson.model)
-coef2=coef(poisson.model2)
-se.coef1=se.coef(poisson.model)
-se.coef2=se.coef(poisson.model2)
-models.both<-cbind(coef1, se.coef1, coef2, se.coef2, exponent=exp(coef1))
+coef1 = coef(fit3)
+coef2 = coef(fit4)
+se.coef1 = se.coef(fit3)
+se.coef2 = se.coef(fit4)
+models.both <- cbind(coef1, se.coef1, coef2, se.coef2, exponent=exp(coef1))
 models.both
 
 
 # Predicting From The Model
-#predict(poisson.model2, newdata = newdata, type = "response")
+#predict(fit4, newdata = newdata, type = "response")
 
 # Visualizing
-plot_summs(poisson.model2, scale = TRUE, exp = TRUE)
-plot_summs(poisson.model, poisson.model2, scale = TRUE, exp = TRUE)
+plot_summs(fit4, scale = TRUE, exp = TRUE)
+plot_summs(fit3, fit4, scale = TRUE, exp = TRUE)
+
+
+
